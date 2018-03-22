151a84868fde9e4619b866aded5424d455d2e52c
LG <glthinkspark@yeah.net>
2018-03-22 18:13:53 +0800

    add java

---
+++ src/lg/java/study/day18IO/FileWriterDemo3.java
@@ -0,0 +1,18 @@
+package lg.java.study.day18IO;
+/*
+演示对已有文件的数据续写。
+*/
+import java.io.*;
+class  FileWriterDemo3
+{
+	public static void main(String[] args) throws IOException
+	{
+
+		//传递一个true参数，代表不覆盖已有的文件。并在已有文件的末尾处进行数据续写。
+		FileWriter fw = new FileWriter("./src/lg/java/study/day18IO/demo.txt",true);
+
+		fw.write("nihao\r\nxiexie");
+
+		fw.close();
+	}
+}
---
+++ src/lg/java/study/day19IO/SystemInfo.java
@@ -0,0 +1,13 @@
+package lg.java.study.day19IO;
+import java.util.*;
+import java.io.*;
+class  SystemInfo
+{
+	public static void main(String[] args) throws IOException
+	{
+		Properties prop = System.getProperties();
+
+		//System.out.println(prop);
+		prop.list(new PrintStream("sysinfo.txt"));
+	}
+}
